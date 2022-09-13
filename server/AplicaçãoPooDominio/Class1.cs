5454af0f591c1e3405e0efa8f72433d57797679a
jarlis santos Almeida <jharlis9@gmail.com>
2022-09-13 20:59:45 -0300

    closet#1

--- server/AplicaçãoPooDominio/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AplicaçãoPooDominio
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ server/AplicaçãoPooDominio/Credito.cs
@@ -0,0 +1,18 @@
+﻿using AplicaçãoPooDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicaçãoPoo.Dominio
+{
+    public class Credito
+    {
+        Pagamento pagamento { get; set }
+
+        int  { get;set; }
+
+        float { get; set;}
+    }
+}

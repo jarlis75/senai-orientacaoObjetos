e47b0d566dd7b0a2076168f580e57dc11963daee
jarlis santos Almeida <jharlis9@gmail.com>
2022-09-13 20:27:13 -0300

    #1- inicializando

---
+++ server/AplicaçãoPooDominio/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace AplicaçãoPooDominio
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ server/AplicaçãoPooDominio/Debito.cs
@@ -0,0 +1,16 @@
+﻿using AplicaçãoPooDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicaçãoPoo.Dominio
+{
+    public class Debito
+    {
+        Pagamento Pagamento { get; set; } 
+        
+        double Desconto { get; set; }
+    }
+}
